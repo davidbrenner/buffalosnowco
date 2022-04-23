bad37bf452967ca8b72fa8abe5aabc9974d72fc9
David Brenner <david.a.brenner@gmail.com>
2022-04-23 18:27:49 -0400

    Pull data from firestore

--- lib/screens/find_jobs.dart
+++ lib/screens/find_jobs.dart
@@ -1,5 +1,7 @@
 import 'package:buffalosnowco/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FindJobsScreen extends StatefulWidget {
   static const routeName = '/jobs';
@@ -10,15 +12,39 @@ class FindJobsScreen extends StatefulWidget {
 }
 
 class _FindJobsScreenState extends State<FindJobsScreen> {
+  final CollectionReference jobs =
+      FirebaseFirestore.instance.collection('jobs');
+  final Stream<QuerySnapshot> _jobsStream =
+      FirebaseFirestore.instance.collection('jobs').snapshots();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: SnowAppBar(),
-        body: Column(
-          children: const [
-            Text("Find Jobs"),
-          ],
+        body: StreamBuilder<QuerySnapshot>(
+          stream: _jobsStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                return ListTile(
+                  title: Text(data['title']),
+                  subtitle: Text(data['description']),
+                );
+              }).toList(),
+            );
+          },
         ),
       ),
     );
