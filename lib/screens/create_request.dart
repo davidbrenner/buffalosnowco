3cd232e6e02c3a85a5ac6f286df0004d73fd60a3
David Brenner <david.a.brenner@gmail.com>
2022-04-24 13:15:13 -0400

    Add padding for CreateRequest Page

--- lib/screens/create_request.dart
+++ lib/screens/create_request.dart
@@ -26,116 +26,121 @@ class _CreateRequestScreenState extends State<CreateRequestScreen> {
       child: Scaffold(
           appBar: SnowAppBar(),
           body: SingleChildScrollView(
-            child: Column(children: [
-              FormBuilder(
-                key: _formKey,
-                child: Column(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(children: [
+                FormBuilder(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      FormBuilderTextField(
+                        name: 'title',
+                        decoration: const InputDecoration(
+                          labelText: 'Title description of issue',
+                        ),
+                        onChanged: (String? string) {},
+                        // valueTransformer: (text) => num.tryParse(text),
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(),
+                          FormBuilderValidators.max(70),
+                        ]),
+                        keyboardType: TextInputType.number,
+                      ),
+                      FormBuilderTextField(
+                        name: 'description',
+                        decoration: const InputDecoration(
+                          labelText:
+                              'A more detailed description of the request/problem',
+                        ),
+                        onChanged: (String? string) {},
+                        // valueTransformer: (text) => num.tryParse(text),
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(),
+                          FormBuilderValidators.max(70),
+                        ]),
+                        keyboardType: TextInputType.number,
+                      ),
+                      FormBuilderTextField(
+                        name: 'address',
+                        decoration: const InputDecoration(
+                          labelText: 'The location of the request',
+                        ),
+                        onChanged: (String? string) {},
+                        // valueTransformer: (text) => num.tryParse(text),
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(),
+                          FormBuilderValidators.max(70),
+                        ]),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
                   children: <Widget>[
-                    FormBuilderTextField(
-                      name: 'title',
-                      decoration: const InputDecoration(
-                        labelText: 'Title description of issue',
+                    Expanded(
+                      child: MaterialButton(
+                        color: Theme.of(context).colorScheme.primary,
+                        child: Text(
+                          "Submit",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () async {
+                          _formKey.currentState!.save();
+
+                          if (_formKey.currentState!.validate()) {
+                            print(_formKey.currentState!.value);
+                            await jobs.add({
+                              'title': _formKey.currentState!.value['title'],
+                              'description':
+                                  _formKey.currentState!.value['description'],
+                              'address':
+                                  _formKey.currentState!.value['address'],
+                              'createdAt': DateTime.now(),
+                              'createdBy':
+                                  FirebaseAuth.instance.currentUser?.uid ??
+                                      "Anonymous",
+                            });
+                            await showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text('Job submitted'),
+                                    content:
+                                        Text('Thanks for submitting a job'),
+                                    actions: <Widget>[
+                                      TextButton(
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text('Close')),
+                                    ],
+                                  );
+                                });
+                          } else {
+                            print("validation failed");
+                          }
+                        },
                       ),
-                      onChanged: (String? string) {},
-                      // valueTransformer: (text) => num.tryParse(text),
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(),
-                        FormBuilderValidators.max(70),
-                      ]),
-                      keyboardType: TextInputType.number,
                     ),
-                    FormBuilderTextField(
-                      name: 'description',
-                      decoration: const InputDecoration(
-                        labelText:
-                            'A more detailed description of the request/problem',
+                    SizedBox(width: 20),
+                    Expanded(
+                      child: MaterialButton(
+                        color: Theme.of(context).colorScheme.secondary,
+                        child: Text(
+                          "Reset",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          _formKey.currentState!.reset();
+                        },
                       ),
-                      onChanged: (String? string) {},
-                      // valueTransformer: (text) => num.tryParse(text),
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(),
-                        FormBuilderValidators.max(70),
-                      ]),
-                      keyboardType: TextInputType.number,
-                    ),
-                    FormBuilderTextField(
-                      name: 'address',
-                      decoration: const InputDecoration(
-                        labelText: 'The location of the request',
-                      ),
-                      onChanged: (String? string) {},
-                      // valueTransformer: (text) => num.tryParse(text),
-                      validator: FormBuilderValidators.compose([
-                        FormBuilderValidators.required(),
-                        FormBuilderValidators.max(70),
-                      ]),
-                      keyboardType: TextInputType.number,
                     ),
                   ],
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: MaterialButton(
-                      color: Theme.of(context).colorScheme.secondary,
-                      child: Text(
-                        "Submit",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () async {
-                        _formKey.currentState!.save();
-
-                        if (_formKey.currentState!.validate()) {
-                          print(_formKey.currentState!.value);
-                          await jobs.add({
-                            'title': _formKey.currentState!.value['title'],
-                            'description':
-                                _formKey.currentState!.value['description'],
-                            'address': _formKey.currentState!.value['address'],
-                            'createdAt': DateTime.now(),
-                            'createdBy':
-                                FirebaseAuth.instance.currentUser?.uid ??
-                                    "Anonymous",
-                          });
-                          await showDialog(
-                              context: context,
-                              builder: (context) {
-                                return AlertDialog(
-                                  title: Text('Job submitted'),
-                                  content: Text('Thanks for submitting a job'),
-                                  actions: <Widget>[
-                                    TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text('Close')),
-                                  ],
-                                );
-                              });
-                        } else {
-                          print("validation failed");
-                        }
-                      },
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                  Expanded(
-                    child: MaterialButton(
-                      color: Theme.of(context).colorScheme.secondary,
-                      child: Text(
-                        "Reset",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      onPressed: () {
-                        _formKey.currentState!.reset();
-                      },
-                    ),
-                  ),
-                ],
-              )
-            ]),
+                )
+              ]),
+            ),
           )),
     );
   }
